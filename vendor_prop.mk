b1216e820238ce9b49d1055b744f20bd5e5a0368
Pedro Kalil <kalilgamer0@gmail.com>
2018-07-06 15:33:32 +0000

    msm8953-common: prop: close the expressions * The expressions werent closed, making them all cluster up in one line

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -134,15 +134,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.rild.nitz_plmn= \
-    persist.rild.nitz_long_ons_0= \
-    persist.rild.nitz_long_ons_1= \
-    persist.rild.nitz_long_ons_2= \
-    persist.rild.nitz_long_ons_3= \
-    persist.rild.nitz_short_ons_0= \
-    persist.rild.nitz_short_ons_1= \
-    persist.rild.nitz_short_ons_2= \
-    persist.rild.nitz_short_ons_3=
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -162,7 +162,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.no_wait_for_card=1 \
     persist.vendor.radio.oem_ind_to_both=0 \
     persist.vendor.radio.relay_oprt_change=1 \
-    rild.libargs=-d /dev/smd0 \
+    rild.libargs="-d /dev/smd0" \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.mot.ignore_csim_appid=true \
     persist.sys.ssr.restart_level=ALL_ENABLE \
