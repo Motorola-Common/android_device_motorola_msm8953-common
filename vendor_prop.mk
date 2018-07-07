03300a93508f9dd0126d58d84f1a6b383bd57427
Pedro Kalil <kalilgamer0@gmail.com>
2018-07-07 20:56:41 +0000

    msm8953-common: decomonize rild.libargs prop * Because the prop contains 2 words instead of one, it doesnt get parsed properly and /dev/smd0 is outputted to a new line * This causes the first sim slot to be duplicated, so your single sim looks like 2 and the second sim doesnt work * A temporary fix for this is adding it back to system.prop in each device again.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -162,7 +162,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.radio.no_wait_for_card=1 \
     persist.vendor.radio.oem_ind_to_both=0 \
     persist.vendor.radio.relay_oprt_change=1 \
-    rild.libargs="-d /dev/smd0" \
     rild.libpath=/system/vendor/lib64/libril-qc-qmi-1.so \
     ro.mot.ignore_csim_appid=true \
     persist.sys.ssr.restart_level=ALL_ENABLE \
